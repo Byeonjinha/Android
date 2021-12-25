253c4abe6b9f3522332b1cf594d3e65ef40f393b
Jinha <jinhaday@gmail.com>
2021-12-25 23:18:46 +0900

    2021/12/25

--- Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
@@ -8,35 +8,57 @@ import java.lang.Math.abs
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    var p_num = 3
+    var k =1
+    val point_list = mutableListOf<Float>()
+
+    fun main(){
         setContentView(R.layout.activity_main)
         var timerTask: Timer? = null
-        var isRunning = false
+        var stage = 1
         var sec : Int = 0
         val tv: TextView = findViewById(R.id.tv_random)
         val tv_t: TextView = findViewById(R.id.tv_timer)
         val tv_p: TextView = findViewById(R.id.tv_point)
+        val tv_people: TextView = findViewById(R.id.tv_people)
         val btn: Button = findViewById(R.id.btn_main)
-
         val random_box = Random()
         val num =random_box.nextInt(1001)
         tv.text = ((num.toFloat())/100).toString()
-
+        btn.text = "시작"
+        tv_people.text = "참가자 $k"
         btn.setOnClickListener{
-            isRunning = !isRunning
-            if (isRunning == true) {
+            stage ++
+            if (stage == 2) {
                 timerTask = kotlin.concurrent.timer(period = 10) {
                     sec++
                     runOnUiThread {
                         tv_t.text = (sec.toFloat()/100).toString()
                     }
                 }
-            } else {
+                btn.text = "정지"
+            } else if(stage == 3) {
                 timerTask?.cancel()
                 val point = abs(sec-num).toFloat()/100
+                point_list.add(point)
+
                 tv_p.text = point.toString()
+                btn.text = "다음"
+                stage = 0
+            }  else if(stage == 1) {
+                if (k< p_num) {
+                    k++
+                    main()
+                } else {
+                    println(k)
+                }
             }
         }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        main()
+
+
     }
 }
